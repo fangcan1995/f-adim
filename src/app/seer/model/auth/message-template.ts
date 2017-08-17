40b637ce638976d5d7e52269c58119a3450a9eb5
liuli <liuli@bbh.com>
2017-08-17 14:08:25 +0800

    first

---
+++ src/app/seer/model/auth/message-template.ts
@@ -0,0 +1,11 @@
+import {BaseEntity} from "../BaseEntity";
+export class Template extends BaseEntity{
+  tplId:string;
+  tplName:string;
+  tplCode:string;
+  tplType:string;
+  tplTitle:string;
+  tplContent:string;
+  tplBusiness:string;
+  tplisSystem:string;
+}
