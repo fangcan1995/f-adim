2fd36b3423f2503ebdca46136a41f1b222be3876
err <err@bbh.com>
2017-09-01 09:22:52 +0800

    添加risk-rating路由

---
+++ src/app/seer/security/risk-rating/components/risk-rating-edit/risk-rating-edit.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  Router,
+  ActivatedRoute,
+} from '@angular/router';
+import { Location } from '@angular/common';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  templateUrl: './risk-rating-edit.component.html',
+  styleUrls: ['./risk-rating-edit.component.scss']
+})
+export class RiskRatingEditComponent implements OnInit {
+  constructor(
+  ) {}
+  ngOnInit() {
+  }
+}
