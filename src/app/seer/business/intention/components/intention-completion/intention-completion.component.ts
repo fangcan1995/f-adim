fdfb46528249d43a99ce28d8a27e39d1dca7ad82
aaron <aaron@aliyun.com>
2017-09-04 09:45:54 +0800

    remove dependence:
    "wintersmith": "2.2.5",
    "wintersmith-sassy": "1.1.0"

---
+++ src/app/seer/business/intention/components/intention-completion/intention-completion.component.ts
@@ -0,0 +1,21 @@
+
+import {Component, OnInit} from "@angular/core";
+@Component({
+  templateUrl: './intention-completion.component.html',
+  styleUrls: ['./intention-completion.component.scss']
+})
+export class IntentionCompletionComponent implements OnInit {
+
+  public member: any = {};
+
+
+  public provices =[]
+
+  constructor(){}
+
+  ngOnInit() {
+
+
+  }
+
+}
