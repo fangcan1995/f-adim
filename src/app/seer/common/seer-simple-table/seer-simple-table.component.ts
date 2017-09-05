46f0aadb9e0ff945ac482a0558edcf4dd2dcf852
err <err@bbh.com>
2017-09-05 09:46:18 +0800

    新建simple table

---
+++ src/app/seer/common/seer-simple-table/seer-simple-table.component.ts
@@ -0,0 +1,23 @@
+import {
+  Component,
+  ViewEncapsulation,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges
+} from '@angular/core';
+
+@Component({
+  selector: 'seer-simple-table',
+  templateUrl: './seer-simple-table.component.html',
+  styleUrls: ['./seer-simple-table.component.scss'],
+})
+export class SeerSimpleTableComponent implements OnInit {
+  constructor() {
+
+  }
+  ngOnInit() {
+
+  }
+}
