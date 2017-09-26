254862e97b76ca22b8c01bd3dbcab25be85e6aec
err <err@bbh.com>
2017-09-26 09:25:42 +0800

    table分页接口

--- src/app/seer/common/seer-content-top/seer-content-top.component.ts
+++ src/app/seer/common/seer-content-top/seer-content-top.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, OnDestroy, Input } from '@angular/core';
 @Component({
   selector: 'seer-content-top',
   templateUrl: './seer-content-top.component.html',
@@ -6,6 +6,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class SeerContentTopComponent{
   @ViewChild('contentTop') contentTop:ElementRef;
+  @Input() fixedTop:boolean = true;
   private _offsetTop:number;
   private _defaultTop:number;
   constructor() {}
@@ -14,10 +15,21 @@ export class SeerContentTopComponent{
   	this._defaultTop = this.contentTop.nativeElement.offsetTop;
   }
   public handleScroll({ direction, scrollY }) {
-    
+    if ( !this.fixedTop ) return this._offsetTop = undefined;
     if ( scrollY > this._defaultTop ) {
     	scrollY = this._defaultTop;
     }
     this._offsetTop = -scrollY;
+  }
+  ngOnInit() {
+    if ( this.fixedTop ) {
+      document.getElementsByClassName('al-content')[0].classList.add('fixedContentTop')
+    } else {
+      document.getElementsByClassName('al-content')[0].classList.remove('fixedContentTop')
+    }
+    
+  }
+  ngOnDestroy() {
+    document.getElementsByClassName('al-content')[0].classList.remove('fixedContentTop')
   }
 }
