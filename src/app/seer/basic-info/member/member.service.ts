de0cc92fd4edf1d40cc54b4d6494b1370fc22d62
Hooly.zz <hooly.zz@aliyun.com>
2017-09-27 19:45:19 +0800

    ZXD

--- src/app/seer/basic-info/member/member.service.ts
+++ src/app/seer/basic-info/member/member.service.ts
@@ -1,17 +1,34 @@
-import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from "@angular/core";
+import {Observable} from 'rxjs/Observable';
 import * as _ from 'lodash';
 import {
   HttpInterceptorService,
 } from '../../../theme/services';
+
 @Injectable()
 export class MemberService {
   mockData = []
+
   constructor(private _httpInterceptorService: HttpInterceptorService) {
-     for ( var i = 0; i < 100; i++ ) {
-       this.mockData.push({"id":"1","name":"本拉登"+ i + "","real_name":"本·拉登","gender":"0","age":"52","marriage":"0","id_number":"123456","mobile":"13912342234","census_register":"阿富汗赫拉特","residence":"阿富汗坎大哈","annual_income":"1,000,000","asset_introduction":"负债1,000,000", "someStatus": "1"})
-     }
+    for (var i = 0; i < 100; i++) {
+      this.mockData.push({
+        "id": "1",
+        "name": "本拉登" + i + "",
+        "real_name": "本·拉登",
+        "gender": "0",
+        "age": "52",
+        "marriage": "0",
+        "id_number": "123456",
+        "mobile": "13912342234",
+        "census_register": "阿富汗赫拉特",
+        "residence": "阿富汗坎大哈",
+        "annual_income": "1,000,000",
+        "asset_introduction": "负债1,000,000",
+        "someStatus": "1"
+      })
+    }
   }
+
   // 获取数据列表
   getList(params?): Observable<any> {
     // return this._httpInterceptorService.request('GET', `${baseUrl}/${apis['MEMBERS']}`, params);
@@ -23,6 +40,7 @@ export class MemberService {
     }
     return Observable.of(res)
   }
+
   // 删除一条数据
   deleteOne(id): Observable<any> {
     // return this._httpInterceptorService.request('DELETE', `${baseUrl}/${apis['MEMBERS']}/${id}`)
@@ -35,6 +53,7 @@ export class MemberService {
     }
     return Observable.of(res);
   }
+
   // 获取一条数据
   getOne(id): Observable<any> {
     // return this._httpInterceptorService.request('GET', `${baseUrl}/${apis['MEMBERS']}/${id}`);
@@ -47,8 +66,9 @@ export class MemberService {
       extras: {}
     }
     return Observable.of(res);
-    
+
   }
+
   // 添加一条数据
   postOne(params): Observable<any> {
     // return this._httpInterceptorService.request('POST', `${baseUrl}/${apis['MEMBERS']}`, params)
@@ -63,11 +83,12 @@ export class MemberService {
     }
     return Observable.of(res);
   }
+
   // 修改一条数据，提供所有字段
   putOne(id, params): Observable<any> {
     // return this._httpInterceptorService.request('PUT', `${baseUrl}/${apis['MEMBERS']}/${id}`, params)
     let index = _.findIndex(this.mockData, t => t.id === id);
-    if ( index != -1 ) {
+    if (index != -1) {
       this.mockData[index] = params;
     }
     let res = {
@@ -78,15 +99,16 @@ export class MemberService {
     }
     return Observable.of(res);
   }
+
   // 修改一条数据，提供部分字段
- /* patchOne(id, params): Observable<any> {
-    // return this._httpInterceptorService.request('PATCH', `${baseUrl}/${apis['MEMBERS']}/${id}`, params)
-    let res = {
-      code: 0,
-      msg: '',
-      data,
-      extras: {}
-    }
-    return Observable.of(res);
-  }*/
+  /* patchOne(id, params): Observable<any> {
+     // return this._httpInterceptorService.request('PATCH', `${baseUrl}/${apis['MEMBERS']}/${id}`, params)
+     let res = {
+       code: 0,
+       msg: '',
+       data,
+       extras: {}
+     }
+     return Observable.of(res);
+   }*/
 }
