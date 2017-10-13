ae89bf00f1126d08eb761636a79477b89cd8cf6e
err <err@bbh.com>
2017-10-13 14:43:22 +0800

    123

--- src/app/seer/operation/coupon/coupon.routing.ts
+++ src/app/seer/operation/coupon/coupon.routing.ts
@@ -2,7 +2,6 @@ import { Routes, RouterModule }  from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { CouponComponent } from './coupon.component';
 import { CouponEditComponent } from './components/coupon-edit/coupon-edit.component';
-import { CouponAddComponent } from './components/coupon-add/coupon-add.component';
 
 const routes: Routes = [
   {
@@ -15,10 +14,6 @@ const routes: Routes = [
     	{
     		path: 'add',
     		component: CouponEditComponent,
-    	},
-    	{
-    		path: 'edit',
-    		component: CouponAddComponent,
     	},
     ]
   }
