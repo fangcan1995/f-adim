dfaf4f545aa84166713cbc80020e0cfa035ccaf4
guifeng <fengyunlingying@qq.com>
2017-10-26 18:37:04 +0800

    message-record.routing.ts

--- src/app/seer/message/message-record/message-record.routing.ts
+++ src/app/seer/message/message-record/message-record.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule }  from '@angular/router';
-
 import { ModuleWithProviders } from '@angular/core';
+
+
 import { MessageRecordComponent } from "./message-record.component";
 
 const routes: Routes = [
