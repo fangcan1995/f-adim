834a07d4e5ac6a74a4df65e138063b94031fee40
Hooly.zz <hooly.zz@aliyun.com>
2017-11-02 13:46:11 +0800

    ZXD

--- src/app/seer/basic-info/staff/staff.config.ts
+++ src/app/seer/basic-info/staff/staff.config.ts
@@ -1,119 +1,38 @@
 export const hasGlobalFilter = true;
 export const filters = [
-  {
-    key: 'staffDtoNo',
-    label: '员工编号',
-    type: 'input.text',
-  },
-  {
-    key: 'staffDtoName',
-    label: '姓名',
-    type: 'input.text',
-  },
-  {
-    key: 'staffDtoCardId',
-    label: '身份证',
-    type: 'input.text',
-  },
+  {key: 'staffDtoNo', label: '员工编号', type: 'input.text'},
+  {key: 'staffDtoName', label: '姓名', type: 'input.text'},
+  {key: 'staffDtoCardId', label: '身份证', type: 'input.text'},
 ];
 export const titles = [
-  {
-    key:'empName',
-    label:'姓名',
-  },
-  {
-    key:'emCode',
-    label:'员工编号',
-  },
-  {
-    key:'pDepartmentName',
-    label:'分公司',
-  },
-  {
-    key:'departmentName',
-    label:'团队',
-  },
-  {
-    key:'position',
-    label:'职位',
-  },
-  {
-    key:'entryTime',
-    label:'入职时间',
-    type: 'date',
-  },
-  {
-    key:'inviteNum',
-    label:'邀请人数',
-  },
-  {
-    key:'loginNum',
-    label:'登录次数',
-  },
-  {
-    key:'lastLoginTime',
-    label:'最后登录时间',
-  },
-  {
-    key:'lastLoginIP',
-    label:'最后登录IP',
-  }
-];;
-export const titlesEducation=[
-  {
-    key:'department',
-    label:'毕业院校',
-  },
-  {
-    key:'eduLevel',
-    label:'所学专业',
-  },
-  {
-    key:'eduMajor',
-    label:'学位',
-  },
-  {
-    key:'endTime',
-    label:'毕业时间',
-  },
+  {key: 'empName', label: '姓名'},
+  {key: 'emCode', label: '员工编号'},
+  {key: 'pDepartmentName', label: '分公司'},
+  {key: 'departmentName', label: '团队'},
+  {key: 'position', label: '职位'},
+  {key: 'entryTime', label: '入职时间', type: 'date'},
+  {key: 'inviteNum', label: '邀请人数'},
+  {key: 'loginTimes', label: '登录次数'},
+  {key: 'lastLoginTime', label: '最后登录时间', type: 'date-time'},
+  {key: 'loginIp', label: '最后登录IP'}
 ];
-export const titlesFamily=[
-  {
-    key:'relation',
-    label:'与本人关系',
-  },
-  {
-    key:'name',
-    label:'姓名',
-  },
-  {
-    key:'work',
-    label:'工作单位及职务',
-  },
-  {
-    key:'phone',
-    label:'联系电话',
-  },
+export const titlesEducation = [
+  {key: 'college', label: '毕业院校'},
+  {key: 'eduMajor', label: '所学专业'},
+  {key: 'eduLevel', label: '学历'},
+  {key: 'endTime', label: '毕业时间'},
 ];
-export const titlesBusinessHistory=[
-  {
-    key:'startAndStopDate',
-    label:'起止日期',
-  },
-  {
-    key:'unit',
-    label:'所学专业',
-  },
-  {
-    key:'position',
-    label:'职务/工种',
-  },
-  {
-    key:'reterence',
-    label:'证明人',
-  },
-  {
-    key:'telephone',
-    label:'联系电话',
-  },
+export const titlesRelation = [
+  {key: 'contRelation', label: '与本人关系'},
+  {key: 'contName', label: '姓名'},
+  {key: 'jobInfo', label: '工作单位及职务'},
+  {key: 'contPhone', label: '联系电话'},
+];
+export const titlesExperience = [
+  {key: 'companyName', label: '公司名称'},
+  {key: 'jobType', label: '职务/工种'},
+  {key: 'proveName', label: '证明人'},
+  {key: 'proveTel', label: '联系电话'},
+  {key: 'startTime', label: '开始日期'},
+  {key: 'endTime', label: '结束日期'}
 ];
