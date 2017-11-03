d69da65abb013e3d24ed97f580c2d5182a38a1bc
guifeng <fengyunlingying@qq.com>
2017-11-03 11:41:47 +0800

    announcement service update

--- src/app/seer/content/announcement/announcement.service.ts
+++ src/app/seer/content/announcement/announcement.service.ts
@@ -23,29 +23,16 @@ export class AnnouncementService extends BaseService<any>{
 
 
   // 获取数据列表
-  /*getList(params?): Promise<any> {
-    const url = `${this.apiUrl}`;
-    return this.getAll(url);
-  }
-*/
   getList(params?): Promise<ResModel> {
     return this._httpInterceptorService.request('GET', 'http://172.16.4.62:8070/announcements', params).toPromise();
   }
 
   // 删除一条数据
-  /*deleteOne (id): Promise<any> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.delete(url);
-  }*/
   deleteOne(id: string | number): Promise<ResModel> {
     return this._httpInterceptorService.request('DELETE', `http://172.16.4.62:8070/announcements/${id}`).toPromise();
   }
 
   // 获取一条数据
-  /*getOne (id): Promise<any> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.getById(url);
-  }*/
   getOne(id: string | number): Promise<ResModel> {
     return this._httpInterceptorService.request('GET', `http://172.16.4.62:8070/announcements/${id}`).toPromise();
   }
@@ -57,11 +44,6 @@ export class AnnouncementService extends BaseService<any>{
   }*/
 
   // 修改一条数据，提供所有字段
-  /*putOne(params): Promise<any> {
-    const url = `${this.apiUrl}?access_token=${this.accessToken}`;
-    return this.update(url, params);
-  }*/
-
   putOne(params: any): Promise<ResModel> {
     return this._httpInterceptorService.request('PUT', `http://172.16.4.62:8070/announcements`, params).toPromise();
   }
