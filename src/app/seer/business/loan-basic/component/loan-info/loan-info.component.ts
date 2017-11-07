3ebac3f75867880553395cdd09c01c15b485b661
aaron <aaron@aliyun.com>
2017-11-07 15:52:36 +0800

    update

--- src/app/seer/business/loan-basic/component/loan-info/loan-info.component.ts
+++ src/app/seer/business/loan-basic/component/loan-info/loan-info.component.ts
@@ -1,8 +1,9 @@
 
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 
 import {LoanBasicService} from "../../loan-basic.service";
 import {SAVE, UPDATE} from "../../../../common/seer-table/seer-table.actions";
+import {SeerMessageService} from "../../../../../theme/services/seer-message.service";
 
 @Component({
   selector: 'loan-info',
@@ -18,15 +19,44 @@ export class LoanInfoComponent implements OnInit {
 
   private actions = [];
 
-  constructor(private service: LoanBasicService){}
+  @Output() notify: EventEmitter<any> = new EventEmitter();
+
+  constructor(private service: LoanBasicService, private _messageService: SeerMessageService,){}
 
   ngOnInit(): void { if(!this.disabled) { this.actions = [ SAVE ] } else { this.actions = [] } }
 
+
+  //保存借款信息
   private save(): void {
     this.service.updateLoan(this.loan).then((res => {
-      console.log(res);
-      alert(res.message);
+      if(0 == res.code) {
+        this.showSuccess(res.msg || '保存成功');
+      } else {
+        this.showError(res.msg || '保存失败');
+      }
     }));
+  }
+
+  //借款类型改变通知
+  private loanTypeChange() {
+    this.notify.emit(this.loan['loanApplyType']);
+
+  }
+
+  showSuccess(message: string) {
+    return this._messageService.open({
+      message,
+      icon: 'fa fa-check',
+      autoHideDuration: 3000,
+    })
+  }
+
+  showError(message: string) {
+    return this._messageService.open({
+      message,
+      icon: 'fa fa-times-circle',
+      autoHideDuration: 3000,
+    })
   }
 
 
