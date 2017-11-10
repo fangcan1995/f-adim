497aba07684762c91c27754c814c93910246f66a
liuli <liuli@bbh.com>
2017-11-10 14:04:55 +0800

    Merge branch 'master' of http://42.202.130.40:8080/bbh_front/bbh-admin
    # Conflicts:
    #	src/app/seer/basic-info/message/components/message-added-dialog/message-added-dialog.component.html
    #	src/app/seer/basic-info/message/components/message-added-dialog/message-added-dialog.component.ts

--- src/app/seer/message/message-template/message-template.service.ts
+++ src/app/seer/message/message-template/message-template.service.ts
@@ -1,15 +1,13 @@
-import {Injectable} from '@angular/core';
-import {SERVER} from "../../const";
-import {BaseService} from "../../base.service";
-import {Result} from "../../model/result.class";
-import {Template} from "../../model/auth/message-template";
-import {parseQueryString, getStorage} from "../../../theme/libs/utils"
+import {Injectable} from "@angular/core";
+import {BaseService,HttpInterceptorService,API,BASE_URL,ResModel} from "../../../theme/services"
+import {getStorage} from "../../../theme/libs/utils"
 @Injectable()
-export class messageTplManageService extends BaseService<any>{
+export class messageTplManageService extends BaseService<ResModel>{
   accessToken = getStorage({ key: 'token' }).access_token;
-  templateManageUrl=`http://172.16.1.234:8080/templates`;  // URL to web api
-  getTpls(pageInfo:any): Promise<Result>{
-    const page=`&pageNum=${pageInfo.pageNum}&pageSize=${pageInfo.pageSize}`;
+  templateManageUrl=`http://172.16.7.3:9010/templates`;  // URL to web api
+
+  getTpls(pageInfo:any): Promise<ResModel> {
+    const page=`?pageNum=${pageInfo.pageNum}&pageSize=${pageInfo.pageSize}`;
     const sort=`&sortBy=${pageInfo.sort}`;
     const jsonQueryObj = pageInfo.query;
     let query:string="";
@@ -18,30 +16,22 @@ export class messageTplManageService extends BaseService<any>{
         query+=`&${prop}=${jsonQueryObj[prop]}`;
       }
     }
-    const url = `${this.templateManageUrl}?access_token=${this.accessToken}${page}${sort}${query}`;
-    console.log(url);
-    return this.getAll(url);
-
+    return this._httpInterceptorService.request('GET', `${this.templateManageUrl}${page}${sort}${query}`,{}, true).toPromise();
   }
   /*根据id返回一条数据*/
-  getTemplateById(id: string): Promise<Result> {
-    const url = `${this.templateManageUrl}/${id}?access_token=${this.accessToken}`;
-    return this.getById(url);
+  getTemplateById(id: string): Promise<ResModel> {
+    return this._httpInterceptorService.request('GET', `${this.templateManageUrl}/${id}`,{}, true).toPromise();
   }
   /*新增*/
-  createTemplate(template:Template): Promise<Result> {
-    const url = `${this.templateManageUrl}?access_token=${this.accessToken}`;
-    return this.create(url,template);
+  createTemplate(params): Promise<ResModel> {
+    return this._httpInterceptorService.request('POST', `${this.templateManageUrl}`, params).toPromise();
   }
   /*修改*/
-  updateTemplate(template:Template): Promise<Result> {
-    const url = `${this.templateManageUrl}?access_token=${this.accessToken}`;
-
-    return this.update(url,template);
+  updateTemplate(params): Promise<ResModel> {
+    return this._httpInterceptorService.request('PUT', `${this.templateManageUrl}`, params).toPromise();
   }
   /*删除*/
-  deleteTemplate(id: string): Promise<Result> {
-    const url = `${this.templateManageUrl}/${id}?access_token=${this.accessToken}`;
-    return this.delete(url);
+  deleteTemplate(id: string): Promise<ResModel> {
+    return this._httpInterceptorService.request('DELETE', `${this.templateManageUrl}/${id}`).toPromise();
   }
 }
