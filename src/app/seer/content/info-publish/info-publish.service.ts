c71f9507788cd784f5ee283078062d6c68d77195
guifeng <fengyunlingying@qq.com>
2017-11-15 14:55:09 +0800

    commit info-publish component

--- src/app/seer/content/info-publish/info-publish.service.ts
+++ src/app/seer/content/info-publish/info-publish.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
-import {BaseService} from "../../base.service";
+import {
+  BaseService,
+  HttpInterceptorService,
+  API,
+  BASE_URL,
+  ResModel,
+} from "../../../theme/services";
 import {SERVER} from "../../const";
 import { User } from "../../model/auth/user";
 import {Result} from "../../model/result.class";
 import * as _ from 'lodash';
 @Injectable()
-export class InfoPublishService             {
+export class InfoPublishService extends BaseService<any>{
     private orgManageUrl = SERVER + '/basicinfo';
     private userManageUrl = SERVER + '/sys/user';
     private roleManageUrl = SERVER+'/sys/role';
     // private headers = new Headers({'Content-Type': 'application/json'});
-    mockData = [
+    /*mockData = [
         {"roleName":"小花","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"01"},
         {"roleName":"小草","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"02"},
         {"roleName":"小人","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"03"},
@@ -26,84 +32,96 @@ export class InfoPublishService             {
         {"roleName":"小狗","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"10"},
         {"roleName":"小蒙","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"09"},
         {"roleName":"小狗","validState":"15800624187","operateTime":"提前还款","number":"333","createTime":"2017-02-07 10:20:28","createUser":"me","sendStage":"已发送","sendDate":"2017-02-07 10:20:28","msgId":"10"}
-    ]
-    constructor(private http: Http, private baseService: BaseService<any>) {
-    }
+    ]*/
+  constructor(
+    protected _httpInterceptorService:HttpInterceptorService
+  ) {
+    super(_httpInterceptorService);
+    this.setApi(API['ORG']);
+  }
 
     private handleError(error: any): Promise<any> {
         console.error('出错啦', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }
-    // 右侧表格假数据
-    getDatas(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      resolve(
-        {
-          'success': true,
-          'data':this.mockData 
-        } 
-      )
-    })
-  }
-    // 删除一条数据
-  deleteOne(id): Observable<any> {
-    // return this._httpInterceptorService.request('DELETE', `${baseUrl}/${apis['MEMBERS']}/${id}`)
-    let data = _.remove(this.mockData, x => x.msgId === id);
-    let res = {
-      code: 0,
-      msg: '',
-      data,
-      extras: {}
-    }
-    return Observable.of(res);
-  }
-     /*
-   * 查询全部库房
-   * */
-    getOrganizations(): Promise<Result> {
-        let url = `${this.orgManageUrl}/organizations`;
-        return this.baseService.getAll(url);
-    }
-    // 用于模态层树的保存调用
-    createUser(user: User): Promise<Result> {
-    const url = `${this.userManageUrl}`;
-    return this.baseService.create(url,user);
-   }
-   
-//   getRoles(): Promise<Result> {
-//     return this.getAll(this.roleManageUrl);
-//   }
-   
+
+
+
+    /* ---------------------------------------------------- */
+    /* 此处为左侧树结构（栏目）模块的方法 */
+    /* ---------------------------------------------------- */
+
     /*
-    * 根据组织id获取员工
-    * */
-    getStaffsByOrgId(orgId): Promise<Result> {
-        let url = `${this.orgManageUrl}/organization/staffs/${orgId}`;
-        return this.baseService.getAll(url);
+     * 查询全部栏目树
+     * */
+    getAllColumnTree(): Promise<ResModel> {
+        return this._httpInterceptorService.request('GET', 'http://172.16.1.221:8070/affiche/type', {}, true).toPromise();
+      }
+
+
+
+
+    /* 添加栏目     params: {}  */
+    addColumn (params): Promise<ResModel> {
+      return this._httpInterceptorService.request('POST', 'http://172.16.1.221:8070/affiche/type', params).toPromise();
     }
 
-    operationRecord(data) {
-        let url = `${this.orgManageUrl}/organization/operation`;
-        return this.baseService.update(url,data);
+    /* 通过栏目id删除栏目   params: id */
+    deleteColumn (id): Promise<ResModel> {
+      return this._httpInterceptorService.request('DELETE', `http://172.16.1.221:8070/affiche/type/${id}`).toPromise();
     }
 
-    updateStaffOrgId(data) {
-        let url = `${this.orgManageUrl}/organization/staff`;
-        return this.baseService.update(url,data);
+    /* 通过栏目id修改栏目   params: {} */
+    editColumn (params):Promise<ResModel> {
+      return this._httpInterceptorService.request('PUT', 'http://172.16.1.221:8070/affiche/type', params).toPromise();
     }
 
-    addOrganization(data) {
-        let url = `${this.orgManageUrl}/organization`;
-        return this.baseService.create(url,data);
-    }
 
-    editOrganization(data) {
-        let url = `${this.orgManageUrl}/organization`;
-        return this.baseService.update(url, data);
-    }
 
-    delOrganization(orgId) {
-        let url = `${this.orgManageUrl}/organization/${orgId}`;
-        return this.baseService.delete(url);
-    }
+
+  /* ---------------------------------------------------- */
+  /* 此处为左侧树结构模块的方法 */
+  /* ---------------------------------------------------- */
+
+  /*根据栏目id获取新闻*/
+  getColumnListById(col_Id): Promise<ResModel> {
+    return this._httpInterceptorService.request('GET', `http://172.16.1.221:8070/affiche/info/${col_Id}`, {}).toPromise();
+  }
+
+  /* 点击新增按钮，添加一条新的数据  params: {}*/
+  addNewArticle (params): Promise<ResModel> {
+    let fileId = params.fileId;
+    return this._httpInterceptorService.request('POST', `http://172.16.1.221:8070/affiche/info/${fileId}`, params).toPromise();
+  }
+
+  /* 点击删除按钮，根据文章id 删除文章  params: id */
+  deleteArticle (id): Promise<ResModel> {
+    return this._httpInterceptorService.request('DELETE', `http://172.16.1.221:8070/affiche/info/${id}`).toPromise();
+  }
+
+  /* 点击 params: {} */
+  editArticle (params): Promise<ResModel> {
+    return this._httpInterceptorService.request('PUT', 'http://172.16.1.221:8070/affiche/info', params).toPromise();
+  }
+
+  /* 点击修改按钮，查询对应id的文章 */
+  getArticle (id): Promise<ResModel> {
+    return this._httpInterceptorService.request('GET', `http://172.16.1.221:8070/affiche/info/${id}`, {}).toPromise();
+  }
+
+  /* 点击修改状态按钮，更改对应id文章的状态 */
+  patch (params): Promise<ResModel> {
+    return this._httpInterceptorService.request('PATCH', 'http://172.16.1.221:8070/affiche/info', params).toPromise();
+  }
+
+
+
+  /*  通过栏目id查询栏目，以及栏目下属的文章信息
+     *    params: {}
+     **/
+  getColumnList (params): Promise<ResModel> {
+    console.log(params);
+    return this._httpInterceptorService.request('GET', 'http://172.16.1.221:8070/affiche/typetoinfo', params).toPromise();
+  }
+
 }
