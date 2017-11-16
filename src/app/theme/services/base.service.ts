9eba848d634546abed765b4e8f29e036c5bae678
Hooly.zz <hooly.zz@aliyun.com>
2017-11-16 16:34:49 +0800

    ZXD

--- src/app/theme/services/base.service.ts
+++ src/app/theme/services/base.service.ts
@@ -9,10 +9,7 @@ import {
 import { getStorage, setStorage, castDict2Translate } from '../libs';
 
 const BASE_DOMAIN = '172.16.7.4';
-// '172.16.1.234'
 const BASE_PORT = 8020;
-// const BASE_DOMAIN = '172.16.1.27';
-// const BASE_PORT = 8090;
 const BASE_SERVER = `${BASE_DOMAIN}:${BASE_PORT}`;
 export const BASE_URL = `http://${BASE_SERVER}`;
 export const API = {
