86dfbd4ae4c55a1b307de4c12b24ccc5d7b292ef
liuli <liuli@bbh.com>
2018-01-03 16:53:37 +0800

    Merge branch 'master' of http://42.202.130.40:8080/bbh_front/bbh-admin
    # Conflicts:
    #	src/app/seer/basic-info/message/components/message-added-dialog/message-added-dialog.component.html
    #	src/app/seer/basic-info/message/components/message-added-dialog/message-added-dialog.component.ts

--- src/app/seer/adver-manage/advertising/advertising.module.ts
+++ src/app/seer/adver-manage/advertising/advertising.module.ts
@@ -7,12 +7,18 @@ import {AdvertisingComponent} from "./advertising.component";
 import {AdverEditComponent} from "./components/adver-edit/adver-edit.component";
 import {AdvertisingService} from "./advertising.service";
 import {FileUploadModule} from "ng2-file-upload";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
+import {NgxCurrencyModule} from "ngx-currency";
 @NgModule({
   imports: [
     NgaModule,
     SharedModule,
     routing,
-    FileUploadModule
+    FileUploadModule,
+    CustomFormsModule,
+    NgxCurrencyModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AdvertisingComponent,
--- src/app/seer/adver-manage/advertising/components/adver-edit/adver-edit.component.ts
+++ src/app/seer/adver-manage/advertising/components/adver-edit/adver-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit,OnChanges,Input} from '@angular/core';
+import {Component, OnInit,OnChanges,Input, ViewChild} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {SeerMessageService} from "../../../../../theme/services/seer-message.service";
 import {AdvertisingService} from "../../advertising.service";
@@ -6,6 +6,7 @@ import {Location} from "@angular/common";
 import {FileUploader, ParsedResponseHeaders, FileItem} from "ng2-file-upload";
 import {getStorage} from "../../../../../theme/libs/utils";
 import {BASE_URL,API} from "../../../../../theme/services/base.service";
+
 import * as _ from 'lodash';
 @Component({
   templateUrl: './adver-edit.component.html',
@@ -18,7 +19,7 @@ export class AdverEditComponent implements OnInit{
   public uploadDisabled:boolean=false;
   public forbidSaveBtn: boolean = true;
   //上传图片相关
-  //fileApi=`http://172.16.1.221:8070/advertising/file`; //上传接口
+
   fileApi=`${BASE_URL}/${API['ADVERTISINGS']}`; //上传接口
   token = getStorage({ key: 'token' });
   tokenType = this.token.token_type;
@@ -26,14 +27,22 @@ export class AdverEditComponent implements OnInit{
   public attachments = [];
   public uploader:FileUploader; //上传对象
   public progress: number = 0; //上传进度
-
+  @ViewChild('validationForm') validationForm;
   constructor(private _advertisingService: AdvertisingService,
               private _messageService: SeerMessageService,
               private _activatedRoute: ActivatedRoute,
               private _router: Router,
               private _location: Location) {
+    //表单验证
+   /* this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      adType: new FormControl('', Validators.required),
+      putEnv: new FormControl('', Validators.required),
+      url: new FormControl('', Validators.required),
+    });*/
   }
   ngOnInit() {
+
     this._activatedRoute.url.mergeMap(url => {
       this._editType = url[0].path;
       return this._activatedRoute.params
@@ -44,7 +53,7 @@ export class AdverEditComponent implements OnInit{
           this._advertisingService.getOne(params.id)
             .then(res => {
               this.advertising = res.data || {};
-              console.log(this.advertising);
+              //console.log(this.advertising);
               this.forbidSaveBtn = false;
               // 初始化定义uploader变量,用来配置input中的uploader属性
               let headers = [{name: 'Authorization', value: `${this.tokenType} ${this.accessToken}`}];
