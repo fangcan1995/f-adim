59440d369946c545159a04b6833a268d48f8e945
guifeng <fengyunlingying@qq.com>
2018-04-09 08:58:13 +0800

    change url

--- src/app/theme/services/base.service.ts
+++ src/app/theme/services/base.service.ts
@@ -11,12 +11,12 @@ import { getStorage, setStorage, castDict2Translate } from '../libs';
 const BASE_DOMAIN = '172.16.7.3';
 const BASE_PORT = 8020;
 const BASE_SERVER = `${BASE_DOMAIN}:${BASE_PORT}`;
-export const BASE_URL = `http://${BASE_SERVER}`;
+export let BASE_URL = `http://${BASE_SERVER}`;
 
 const TEST_DOMAIN = '172.16.1.234';
 const TEST_PORT = 9080;
 const TEST_SERVER = `${TEST_DOMAIN}:${TEST_PORT}`;
-export const TEST_URL = `http://${TEST_SERVER}`;
+export let TEST_URL = `http://${TEST_SERVER}`;
 
 
 
