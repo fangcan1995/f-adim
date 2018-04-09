0e0660801f76b1dbb4594854a1423943448ded40
liliangyu <liliangyu@baba88.com>
2018-04-09 09:33:39 +0800

    修改路径

--- src/app/seer/basic-info/member/member.service.ts
+++ src/app/seer/basic-info/member/member.service.ts
@@ -3,7 +3,7 @@ import {BaseService,HttpInterceptorService,API,BASE_URL,ResModel,TEST_URL} from 
 import {getStorage} from "../../../theme/libs/utils"
 @Injectable()
 export class MemberService extends BaseService<ResModel>{
-   MembersUrl=`${TEST_URL}/${API['MEMBERS']}/members`; //会员接口
+   MembersUrl=`${TEST_URL}/${API['MEMBERS']}`; //会员接口
   //MembersUrl=`http://172.16.1.225:9080/members`; //会员接口
   emergencyContactUrl=`contact`;//联系人
   VehicleContactUrl=`car`;//车辆
