272a48679001eeff18043729c74e5187a1d069b1
fangcan <fangcan@baba88.com>
2018-04-25 09:13:11 +0800

    定时任务完成

--- src/app/seer/system/plan/plan.module.ts
+++ src/app/seer/system/plan/plan.module.ts
@@ -3,23 +3,29 @@ import { NgaModule } from "../../../theme/nga.module";
 import { SharedModule } from "../../common/shared.module";
 
 import { routing } from "./plan.routing";
-import { UserService } from "./plan.service";
+import { PlanService } from "./plan.service";
 import { UserComponent } from "./plan.component";
 import { UserEditComponent } from "./components/plan-edit/plan-edit.component";
 import { RoleService } from "../role/role.service";
 import { ResourceService } from '../resource/resource.service';
+
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
 @NgModule({
   imports: [
     NgaModule,
     SharedModule,
     routing,
+    CustomFormsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     UserComponent,
     UserEditComponent,
   ],
   providers: [
-    UserService,
+    PlanService,
     RoleService,
     ResourceService,
   ]
--- src/app/seer/system/plan/plan.service.ts
+++ src/app/seer/system/plan/plan.service.ts
@@ -19,7 +19,7 @@ const exportFile = `${TEST_URL}/${API['USERS']}/specialExport1`;
 @Injectable()
 
 
-export class UserService extends BaseService<any> {
+export class PlanService extends BaseService<any> {
 
     constructor(
         protected _httpInterceptorService: HttpInterceptorService,
@@ -27,7 +27,7 @@ export class UserService extends BaseService<any> {
         private http: Http
     ) {
         super(_httpInterceptorService);
-        this.setApi(API['USERS']);
+        this.setApi(API['PLAN']);
     }
     getRoles() {
         return this._roleService.getList({ pageSize: 10000 })
@@ -40,8 +40,37 @@ export class UserService extends BaseService<any> {
         //return this._httpInterceptorService.request('PUT', `${BASE_URL}/${API['USERS']}/password`, params).toPromise();
         return this._httpInterceptorService.request('PUT', `${TEST_URL}/${API['USERS']}/password`, params).toPromise();
     }
+    // 新增一条记录
+    postOne(params: any): Promise<ResModel> {
+        return this._httpInterceptorService.request('POST', `${TEST_URL}/quartz`, params).toPromise();
+    }
+    //暂停一个任务
+    putOne(params): Promise<ResModel>{
+        return this._httpInterceptorService.request('PUT', `${TEST_URL}/quartz/pause`, params).toPromise();
+    }
+     //修改一个任务
+     resetOne(params): Promise<ResModel>{
+        return this._httpInterceptorService.request('PUT', `${TEST_URL}/quartz`, params).toPromise();
+    }
+    //恢复一个任务
+    resumeOne(params): Promise<ResModel>{
+        return this._httpInterceptorService.request('PUT', `${TEST_URL}/quartz/resume`, params).toPromise();
+    }
+    // 删除一条任务
+    deleteOne(params): Promise<ResModel> {
+        return this._httpInterceptorService.request('DELETE', `${TEST_URL}/quartz`,params).toPromise();
+        //return this._httpInterceptorService.request('DELETE', `${dictUrl}/${id}`).toPromise();
+    }
+    // 获取列表
+    getList(params?: any): Promise<ResModel> {
+        return this._httpInterceptorService.request('GET', `${TEST_URL}/quartz/all`, params).toPromise();
+    }
 
-
+    // 获取详细
+    getDetail(params?: any): Promise<ResModel> {
+        params=JSON.parse(params)
+        return this._httpInterceptorService.request('GET', `${TEST_URL}/quartz/detail`, params).toPromise();
+    }
     exportForm(params): Promise<any> {
         const access_token = getStorage({ key: 'token' }).access_token;
         return this.http.get(`${exportFile}?access_token=${access_token}`, new RequestOptions({
