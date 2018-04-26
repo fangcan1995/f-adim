1abb7e2b4af4b65de7aacb40812f83d3c93b99ae
linzhiqiang <844145131@qq.com>
2018-04-26 15:05:45 +0800

    添加专题模块

---
+++ src/app/seer/content/subject/subject.component.ts
@@ -0,0 +1,217 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { SubjectService } from "./subject.service";
+import { SeerDialogService } from "../../../theme/services/seer-dialog.service";
+import { SeerMessageService } from "../../../theme/services/seer-message.service";
+import { UPDATE, DELETE, ENABLE, DISABLE } from "../../common/seer-table/seer-table.actions";
+import { ActivatedRoute, Router } from "@angular/router";
+import { formatDate } from "ngx-bootstrap/bs-moment/format";
+import * as _ from 'lodash';
+import {
+    FROUNT_URL
+} from "../../../theme/services";
+
+@Component({
+    templateUrl: './subject.component.html',
+    styleUrls: ['./subject.component.scss']
+})
+export class SubjectComponent implements OnInit, OnDestroy {
+
+    hasGlobalFilter = true;
+    isLoading = true;
+    filterRowLength = 3;
+    filters = [
+        { key: 'subjectName', label: '专题标题', type: 'input.text' },
+        {
+            key: 'subjectType',
+            label: '专题类型',
+            isDict: true,
+            type: 'select',
+            category: 'SUBJECT_TYPE',
+        },
+        {
+            key: 'device',
+            label: '设备',
+            isDict: true,
+            type: 'select',
+            category: 'ADVERTISING_PUTENV',
+        },
+        {
+            key: 'delFlag',
+            label: '专题状态',
+            isDict: true,
+            type: 'select',
+            category: 'DICT_DEL_FLAG',
+        },
+    ];
+
+    subjects = [];
+    titles = [
+        { key: 'subjectName', label: '专题标题' },
+        { key: 'url', label: '专题路径',type:'link' },
+        { key: 'updateTime', label: '修改时间' },
+        { key: 'updateUser', label: '最后修改人' },
+        {
+            key: 'subjectType',
+            label: '专题类型',
+            isDict: true,
+            category: 'SUBJECT_TYPE'
+        },
+        {
+            key: 'device',
+            label: '设备',
+            isDict: true,
+            category: 'ADVERTISING_PUTENV'
+        },
+        {
+            key: 'delFlag',
+            label: '专题状态',
+            isDict: true,
+            category: 'DICT_DEL_FLAG'
+        },
+    ];
+
+
+    pageInfo = {
+        "pageNum": 1,
+        "pageSize": 10,
+        "globalSearch": '',
+        "sortBy": "",
+        "subjectName": '',
+        "subjectType": '',
+        "device": '',
+        "delFlag": '',
+        "total": ''
+    };
+
+    map = {
+
+    }
+
+    constructor(private services: SubjectService, private _dialogService: SeerDialogService,
+        private _messageService: SeerMessageService,
+        private _router: Router, private _activatedRoute: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        this.getList(this.pageInfo);
+    }
+
+
+    getList(params?) {
+        this.services.getList(params)
+            .then(res => {
+                this.pageInfo.pageNum = res.data.pageNum;
+                this.pageInfo.pageSize = res.data.pageSize;
+                this.pageInfo.total = res.data.total;
+                this.subjects = res.data.list;
+                this.subjects = _.map(this.subjects, t => {
+                    let status = t.delFlag;
+                    t.url=`${FROUNT_URL}/${t.id}`
+                    let actions;
+                    switch (status) {
+                        case 0:
+                            actions = [UPDATE, DISABLE];
+                            break;
+                        case 1:
+                            actions = [UPDATE, ENABLE];
+                            break;    
+                    }
+                    return _.set(t, 'actions', actions);
+                })
+                this.isLoading = false;
+            }).catch(err => {
+                this.isLoading = false;
+            })
+    }
+
+    onChange(message) {
+        console.log(message);
+        const type = message.type;
+        let data = message.data;
+        let column = message.column;
+
+        switch (type) {
+            case 'create':
+                this._router.navigate(['add'], { relativeTo: this._activatedRoute });
+                break;
+            case 'update':
+                this._router.navigate([`edit/${message.data.id}`], { relativeTo: this._activatedRoute });
+                break;
+
+                case DISABLE.type:
+                this._dialogService.confirm('确定停用该专题吗？')
+                    .subscribe(action => {
+                        if (action === 1) {
+                            this.services
+                                .deleteOne(data.id)
+                                .then((res) => {
+                                    this.alertSuccess(res.msg || '停用专题成功');
+                                    this.getList(this.pageInfo);
+
+
+                                })
+                                .catch(err => {
+                                    this.alertError(err.msg || '停用专题失败')
+                                });
+                        }
+                    })
+                break;
+            case ENABLE.type:
+                this._dialogService.confirm('确定启用该专题吗？')
+                    .subscribe(action => {
+                        if (action === 1) {
+                            this.services
+                                .deleteOne(data.id)
+                                .then((res) => {
+                                    this.alertSuccess(res.msg || '启用专题成功');
+                                    this.getList(this.pageInfo);
+                                })
+                                .catch(err => {
+                                    this.alertError(err.msg || '启用专题失败')
+                                });
+                        }
+                    })
+                break;
+        }
+    }
+
+    handleFiltersChanged($event) {
+        console.log($event);
+        this.pageInfo.globalSearch = $event.globalSearch;
+        this.pageInfo.subjectName = $event.subjectName;
+        this.pageInfo.subjectType = $event.subjectType;
+        this.pageInfo.device = $event.device;
+        this.pageInfo.delFlag = $event.delFlag;
+        this.getList(this.pageInfo);
+    }
+
+
+    /* 切换页面 */
+    handleChangePage($event) {
+        this.pageInfo.pageSize = $event.pageSize;
+        this.pageInfo.pageNum = $event.pageNum;
+        this.getList(this.pageInfo);
+    }
+
+    ngOnDestroy(): void {
+        // throw new Error("Method not implemented.");
+    }
+
+    alertSuccess(info: string) {
+        return this._messageService.open({
+            icon: 'fa fa-check',
+            message: info,
+            autoHideDuration: 3000,
+        })
+    };
+    alertError(errMsg: string) {
+        // 错误处理的正确打开方式
+        this._messageService.open({
+            icon: 'fa fa-times-circle',
+            message: errMsg,
+            autoHideDuration: 3000,
+        })
+    };
+
+}
---
+++ src/app/seer/content/subject/subject.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from "@angular/core";
+import {
+    BaseService,
+    HttpInterceptorService,
+    API,
+    BASE_URL,
+    ResModel,
+    TEST_URL
+} from "../../../theme/services";
+import { Observable } from "rxjs/Observable";
+
+import { Headers, Http, RequestOptions, ResponseContentType } from '@angular/http';
+import { getStorage } from "../../../theme/libs/utils"
+
+import * as _ from 'lodash';
+
+@Injectable()
+export class SubjectService extends BaseService<any>{
+
+    constructor(
+        protected _httpInterceptorService: HttpInterceptorService,
+        private http: Http
+    ) {
+        super(_httpInterceptorService);
+    }
+
+    const
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+    // apiUrl = `${TEST_URL}/${API['ANNOUNCE']}`;
+    apiUrl = `http://172.16.1.252:9080/subjects`;
+    exportFile = `${TEST_URL}/${API['ANNOUNCE']}/export`;
+    SPExportFile = `${TEST_URL}/${API['ANNOUNCE']}/specialExport`;
+    //SPExportFile = `http://172.16.1.252:9080/announcements/specialExport`;
+    access_token = "access_token=cd59f8af-e2c7-4e21-958f-cbf0747e701a"
+
+
+
+
+
+
+    // 获取数据列表
+    getList(params?): Promise<ResModel> {
+         return this._httpInterceptorService.testRequest('GET', `${this.apiUrl}?${this.access_token}`, params).toPromise();
+
+    }
+
+    // 删除一条数据
+    deleteOne(id: string | number): Promise<ResModel> {
+        return this._httpInterceptorService.testRequest('DELETE', `${this.apiUrl}/${id}?${this.access_token}`).toPromise();
+    }
+
+    // 获取一条数据
+    getOne(id: string | number): Promise<ResModel> {
+        return this._httpInterceptorService.testRequest('GET', `${this.apiUrl}/${id}?${this.access_token}`).toPromise();
+    }
+
+    // 添加一条数据
+    /* postOne(params): Promise<any> {
+       const url = `${this.apiUrl}?access_token=${this.accessToken}`;
+       return this.create(url,params);
+     }*/
+
+    // 修改一条数据，提供所有字段
+    putOne(params: any): Promise<ResModel> {
+        return this._httpInterceptorService.testRequest('PUT', `${this.apiUrl}?${this.access_token}`, params).toPromise();
+    }
+
+    /* 导出表格 */
+    /*exportForm(params: any): Promise<ResModel> {
+      return this._httpInterceptorService.request('POST', `http://172.16.4.62:8070/announcements/export`, params).toPromise();
+    }*/
+
+    exportForm(params): Promise<any> {
+        const access_token = getStorage({ key: 'token' }).access_token;
+        return this.http.get(`${this.exportFile}?access_token=${access_token}`, new RequestOptions({
+            responseType: ResponseContentType.Blob,
+            search: params
+        })).toPromise();
+    }
+
+    specialExportForm(params): Promise<any> {
+        const access_token = getStorage({ key: 'token' }).access_token;
+        return this.http.get(`${this.SPExportFile}?access_token=${access_token}`, new RequestOptions({
+            responseType: ResponseContentType.Blob,
+            search: params
+        })).toPromise();
+    }
+
+
+}
