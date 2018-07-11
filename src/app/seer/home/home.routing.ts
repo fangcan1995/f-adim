11896666a4f1b1e41b11d7fd1abec70291d47bb9
linzhiqiang <844145131@qq.com>
2018-07-11 08:33:23 +0800

    添加首页

--- src/app/seer/home/home.routing.ts
+++ src/app/seer/home/home.routing.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './home.component';
+
 const routes: Routes = [
   {
     path: '',
